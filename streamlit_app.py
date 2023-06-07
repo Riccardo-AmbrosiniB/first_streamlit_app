469791f5224822e19007ccd93c2d8bec7c0b6fab
Riccardo-AmbrosiniB <61760259+Riccardo-AmbrosiniB@users.noreply.github.com>
2023-06-07 16:35:37 +0200

    stop query execution

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('New healthy diner')
 
@@ -31,6 +32,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # tabular response
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
@@ -40,5 +42,7 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('added: ', add_my_fruit)
+
+
 
 my_cur.execute("insert into fruit_load_list values('from_streamlit')")
