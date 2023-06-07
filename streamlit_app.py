c5d98fdd13f8eff9fc74947fe165bde40a16e2dd
Riccardo-AmbrosiniB <61760259+Riccardo-AmbrosiniB@users.noreply.github.com>
2023-06-07 16:31:37 +0200

    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,4 +41,4 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('added: ', add_my_fruit)
 
-my_cur.executre('insert into fruit_load_list values("from_streamlit")')
+my_cur.execute('insert into fruit_load_list values("from_streamlit")')
